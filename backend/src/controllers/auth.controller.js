fbaa58ee12cf7695e8e80124eae263b6eb93b7cd
Dinith Pamunuwatta <dinithchamo@gmail.com>
2026-04-10 23:07:29 +0530

    feat: enhance signup and login functionality with validation and error handling

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,3 @@
-import jwt from 'jsonwebtoken';
 import { generateToken } from '../lib/utils.js';
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
@@ -6,6 +5,9 @@ import bcrypt from 'bcryptjs';
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body;
     try{
+        if(!fullName || !email || !password){
+            return res.status(400).json({message:"Please fill all the fields"});
+        }
         //hash the password
         if(password.length < 6){
             return res.status(400).json({message:"Password must be at least 6 characters long"});
@@ -27,7 +29,7 @@ export const signup = async (req, res) => {
             const token = generateToken(newUser._id, res);
             await newUser.save();
 
-            res.stats(201).json({
+            res.status(201).json({
                 _id: newUser._id,
                 fullName: newUser.fullName,
                 email: newUser.email,
@@ -45,10 +47,45 @@ export const signup = async (req, res) => {
     }
 };
 
-export const login = (req, res) => {
-    res.send("Login route");
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    try{
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+        
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+        //generate JWT token here
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+
+    }
+    catch(error){
+        console.error("Error in login controller:", error);
+        res.status(500).json({message:"Error in login controller"});
+    }
 };
 
 export const logout = (req, res) => {
-    res.send("Logout route");
+    try{
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message:"Logged out successfully"});
+    }
+    catch(error){
+        console.error("Error in logout controller:", error);
+        res.status(500).json({message:"Error in logout controller"});
+    }
+};
+export const updateProfile = async (req, res) => {
+    
 };
