d56d74f4c68ce065b40e5337bf7ef1408c346cdf
Dinith Pamunuwatta <dinithchamo@gmail.com>
2026-04-10 23:07:48 +0530

    feat: add update profile route with authentication middleware

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { signup, login, logout } from '../controllers/auth.controller.js';
+import { signup, login, logout,updateProfile } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const route = express.Router();
 
 route.post("/signup", signup);
 route.post("/login", login);
 route.post("/logout", logout);
-
+route.put("/update-profile",protectRoute, updateProfile);
 
  
 
