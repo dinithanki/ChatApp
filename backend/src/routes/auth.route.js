a4b2edf58de83c7960cede6cb8fcb7965e0604c9
Dinith Pamunuwatta <dinithchamo@gmail.com>
2026-04-11 20:30:02 +0530

    feat: add checkAuth endpoint to auth routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, login, logout,updateProfile } from '../controllers/auth.controller.js';
+import { checkAuth,signup, login, logout,updateProfile } from '../controllers/auth.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 
 const route = express.Router();
@@ -8,7 +8,7 @@ route.post("/signup", signup);
 route.post("/login", login);
 route.post("/logout", logout);
 route.put("/update-profile",protectRoute, updateProfile);
-
+route.get("/check",protectRoute,checkAuth);
  
 
 export default route;
