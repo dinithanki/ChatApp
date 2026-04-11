5e656747dc6713d39c26d4fc5d45cf5f103bcbbf
Dinith Pamunuwatta <dinithchamo@gmail.com>
2026-04-11 20:05:51 +0530

    feat: update auth middleware to use jwt from cookies for token validation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import {connectDB}  from './lib/db.js';
 
-;
 
 dotenv.config();
 const app = express();
@@ -11,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 
