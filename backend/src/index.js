1cbfaf11d118f6f43f49238343d035dc2fa88f1b
Dinith Pamunuwatta <dinithchamo@gmail.com>
2026-04-12 10:54:31 +0530

    feat: add message routes to the main application

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 import {connectDB}  from './lib/db.js';
 
 
@@ -13,7 +14,8 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message",messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
