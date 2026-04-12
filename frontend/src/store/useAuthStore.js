561de3856c31c64dc150a8aaec1be05e4ec1997d
Dinith Pamunuwatta <dinithchamo@gmail.com>
2026-04-12 14:49:22 +0530

    feat: add SignUpPage component and useAuthStore for authentication management

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,26 @@
+import {create} from 'zustand'
+import {axiosInstance} from '../lib/axios.js'
+
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+
+    isCheckingAuth: true, 
+
+    checkAuth: async () => {
+        try{
+            const res = await axiosInstance.get('/auth/check');
+
+            set({authUser: res.data.user, isCheckingAuth: false})
+        }
+        catch(error){
+            console.error('Error checking auth:', error);
+            set({authUser: null})
+        }
+        finally{
+            set({isCheckingAuth: false});
+        }
+    }
+}));
