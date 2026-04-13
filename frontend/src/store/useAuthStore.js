0063aafc8a7306af87eb83baa7c50cbdd20ba017
Dinith Pamunuwatta <dinithchamo@gmail.com>
2026-04-13 18:35:03 +0530

    feat: implement profile picture upload and update functionality in ProfilePage

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -67,5 +67,22 @@ export const useAuthStore = create((set) => ({
         catch(error){
             toast.error("Failed to log out");
         }
+    },
+
+    updateProfile: async (data) => {
+        set({isUpdatingProfile: true});
+        try{
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            set({authUser: res.data});
+            toast.success("Profile updated successfully!");
+        }
+        catch(error){
+            toast.error(error.response?.data?.message || "Failed to update profile");
+        }
+        finally{
+            set({isUpdatingProfile: false});
+        }
+
+
     }
 }));
