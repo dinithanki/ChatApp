6024aed1f7d498dbd72885e3d63c7abfc95fd3db
Dinith Pamunuwatta <155564826+dinithanki@users.noreply.github.com>
2026-05-05 19:09:02 +0530

    fix: correct formatting and endpoint path for message routes in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-import {connectDB}  from './lib/db.js';
-import cors from 'cors';
-
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors(
-  { origin: 'http://localhost:5173',
-    credentials: true,
-  }
-));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 const PORT = process.env.PORT || 5000;
 
@@ -23,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message",messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
