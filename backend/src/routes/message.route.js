5a627dcc6df242573e46eb632d2d3c13db2b1b08
Dinith Pamunuwatta <155564826+dinithanki@users.noreply.github.com>
2026-05-12 11:34:19 +0530

    feat: add route for recent conversations retrieval in message controller

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,16 @@
-import express from 'express';
-import { protectRoute } from '../middleware/auth.middleware.js';
-import { getUsersForSidebar,getMessages,sendMessage } from '../controllers/message.controller.js';
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  getUsersForSidebar,
+  getConversations,
+  getMessages,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.get("/users",protectRoute,getUsersForSidebar);
-router.get("/:id",protectRoute,getMessages);
-router.post("/send/:id",protectRoute,sendMessage);
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/conversations/recent", protectRoute, getConversations);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 export default router;
