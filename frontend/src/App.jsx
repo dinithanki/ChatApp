1ccc14f55cae6f45ffd0e5ec12110c5416ac1dbb
Dinith Pamunuwatta <155564826+dinithanki@users.noreply.github.com>
2026-05-12 11:34:23 +0530

    refactor: improve code formatting and readability in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,63 @@
-import Navbar from "./components/Navbar"
-import { Routes,Route } from "react-router-dom"
-import HomePage from "./pages/HomePage.jsx"
-import SignUpPage from "./pages/SignUpPage"
-import LoginPage from "./pages/LoginPage"
-import SettingPage from "./pages/SettingPage"
-import ProfilePage from "./pages/ProfilePage"
-import { useAuthStore } from "./store/useAuthStore.js"
-import { useEffect } from "react"
-import {Loader} from "lucide-react"
-import { Navigate } from "react-router-dom"
-import { Toaster } from "react-hot-toast"
-import { useThemeStore } from "./store/useThemeStore.js"
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingPage from "./pages/SettingPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore.js";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
+import { Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore.js";
 
 const App = () => {
-  const {authUser,checkAuth,isCheckingAuth} = useAuthStore();
-  const {theme} = useThemeStore();
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
+
+  console.log({ onlineUsers });
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
-  console.log({authUser});
+  console.log({ authUser });
 
-  if(isCheckingAuth && !authUser){
-    return(
+  if (isCheckingAuth && !authUser) {
+    return (
       <div className="flex items-center justify-center h-screen">
-        <Loader className="size-10 animate-spin"/>
+        <Loader className="size-10 animate-spin" />
       </div>
-    )
-  } 
+    );
+  }
 
   return (
     <div data-theme={theme}>
-
-      <Navbar/>
+      <Navbar />
 
       <Routes>
-
-        <Route path="/" element={authUser ? <HomePage /> :<Navigate to="/login" />} />
-        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
         <Route path="/settings" element={<SettingPage />} />
-        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
-
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
       </Routes>
-      <Toaster/>
+      <Toaster />
+    </div>
+  );
+};
 
-     </div>
-  )
-}
-
-export default App
+export default App;
